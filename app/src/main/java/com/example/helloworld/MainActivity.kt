d8fd1904c499d564dccf18bddb73d05f761a0ada
dr-fisk <angelgarcia112198@gmail.com>
2023-07-02 20:12:50 -0600

    Added more improvements

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,22 +2,44 @@ package com.example.helloworld
 
 import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,12 +48,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UiState : ViewModel()
 {
     var messageText by mutableStateOf("")
+    var messageScrollState by mutableStateOf(0)
 }
 
 class MainActivity : ComponentActivity()
@@ -52,7 +80,8 @@ class MainActivity : ComponentActivity()
         Box(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.95f)
+                .fillMaxHeight(0.88f)
+                .padding(bottom = 10.dp)
                 .background(Color.Black)
                 .clickable(
                     onClick = { closeKeyboard(context) },
@@ -71,114 +100,84 @@ class MainActivity : ComponentActivity()
         val newLine = 10
         return (((minKeyCode <= keycode) && (maxKeyCode >= keycode)) || (newLine == keycode))
     }
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        if (event == null)
-        {
-            return false
-        }
-
-        if (event.action == KeyEvent.ACTION_UP)
-        {
-
-            if (isChar(event.getUnicodeChar(event.metaState)))
-            {
-                mUiState.value!!.messageText += event.getUnicodeChar(event.metaState).toChar()
-            }
-            else
-            {
-                // Delete key is pressed, remove last character if string is not empty
-                if (event.keyCode == KeyEvent.KEYCODE_DEL &&
-                    mUiState.value!!.messageText.isNotEmpty() )
-                {
-                    mUiState.value!!.messageText = mUiState.value!!.messageText.dropLast(1)
-                }
-            }
-        }
-
-        return super.onKeyUp(keyCode, event)
-    }
 
     @Composable
     fun TextBox(context : Context)
     {
-        var textStr = "Text Message"
+        var width = 0.85f
+        var height = 50.dp
 
-        // Message has been typed so show it on the UI
         if (mUiState.value!!.messageText.isNotEmpty())
         {
-            textStr = mUiState.value!!.messageText
+            width = 1.0f
+            height = 100.dp
         }
 
-        Box(
-            Modifier
-                .fillMaxWidth(0.80f)
-                .fillMaxHeight()
-                .background(Color(10, 10, 10))
-                .clickable(onClick = { openKeyboard(context) })
-        ) {
-            Text(text = textStr, color = Color.White)
-        }
+        TextField(value = mUiState.value!!.messageText,
+                  onValueChange = { mUiState.value!!.messageText = it },
+                  placeholder = { Text("Text Message")}, shape = CircleShape,
+                  colors = TextFieldDefaults.textFieldColors(
+                      containerColor = Color(10,10,10),
+                      textColor = Color.White,
+                      cursorColor = Color.Gray,
+                      placeholderColor = Color.Gray,
+                      focusedIndicatorColor = Color.Transparent,
+                      unfocusedIndicatorColor = Color.Transparent,
+                      disabledIndicatorColor = Color.Transparent),
+                  modifier = Modifier .fillMaxWidth(width) .height(height),
+                  singleLine = false,
+                  maxLines = 3)
     }
 
     @Composable
     fun MainChat(context: Context) {
-        Row ( horizontalArrangement = Arrangement.SpaceBetween ){
-//        Column {
-//            Box(
-//                Modifier
-//                    .fillMaxWidth(0.15f)
-//                    .fillMaxHeight()
-//                    .background(Color.DarkGray)
-//            ){
-//                Text(text = "List of chats")
-//            }
-//        }
-            Column(verticalArrangement = Arrangement.Top) {
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .padding(10.dp))
+        {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(
+                        rememberScrollState()
+                    )
+                    .height(IntrinsicSize.Max)
+                    .width(IntrinsicSize.Max)
+                    .background(color = Color.Black)
+                    .padding(16.dp)
+            ) {
                 ChatScreen(context)
-                Row(Modifier .fillMaxHeight()) {
-                    Box(
-                        Modifier
-                            .width(30.dp)
-                            .fillMaxHeight()
-                            .background(Color(10, 10, 10))
-                    ) {
-                        Text(text = "Box", color = Color.White)
-                    }
-                    Box(
-                        Modifier
-                            .width(10.dp)
-                            .fillMaxHeight()
-                            .background(Color.Black)
-                    )
-                    TextBox(context)
-                    Box(
-                        Modifier
-                            .width(10.dp)
-                            .fillMaxHeight()
-                            .background(Color.Black)
-                    )
-                    Box(
-                        Modifier
-                            .width(30.dp)
-                            .fillMaxHeight()
-                            .background(Color(10, 10, 10))
-                    ) {
-                        Text(text = "Misc", color = Color.White)
-                    }
-
-                    Box(
-                        Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .background(Color.Black)
-                    )
-                }
-                Box(
+                Row(
                     Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .background(Color.Black)
-                )
+                        .height(IntrinsicSize.Max)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    if (mUiState.value!!.messageText.isEmpty())
+                    {
+                        Box(
+                            Modifier
+                                .width(40.dp)
+                                .height(50.dp)
+                                .offset(x = -10.dp)
+                                .background(Color(10, 10, 10), CircleShape)
+                        ) {
+                            Text(text = "Box", color = Color.White)
+                        }
+                    }
+                    TextBox(context)
+                    if (mUiState.value!!.messageText.isEmpty())
+                    {
+                        Box(
+                            Modifier
+                                .width(40.dp)
+                                .height(50.dp)
+                                .offset(x = 10.dp)
+                                .background(Color(10, 10, 10), CircleShape)
+                        ) {
+                            Text(text = "Misc", color = Color.White)
+                        }
+                    }
+                }
             }
         }
     }
@@ -196,10 +195,12 @@ class MainActivity : ComponentActivity()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         super.onCreate(savedInstanceState)
         val context: Context = this
         setContent {
             MainChat(context)
         }
+        ScrollView(this)
     }
 }
